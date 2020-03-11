6d5a8f37271b64bbd4790e48df885f816ec588e4
Lester Young <lyoung070789@gmail.com>
2020-03-11 16:05:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
       WHERE grade = 10 LIMIT ?
     SQL
     
-    DB[:conn].execute(sql, self.num).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
     
