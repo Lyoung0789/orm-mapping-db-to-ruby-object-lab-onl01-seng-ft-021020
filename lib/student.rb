fd6c8fb6fb3cf6105ccd457f3c76f98316e78b45
Lester Young <lyoung070789@gmail.com>
2020-03-11 16:15:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ class Student
       WHERE grade = 10 LIMIT 1
     SQL
     # binding.pry 
-    DB[:conn].execute(sql, num).map do |row|
+    DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
     end
   end   
