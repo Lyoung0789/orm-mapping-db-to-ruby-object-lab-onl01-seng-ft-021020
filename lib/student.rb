2cb43ec9c6bd18b6f45d17df211d1b30276a1e56
Lester Young <lyoung070789@gmail.com>
2020-03-11 16:02:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,12 +54,12 @@ class Student
       WHERE grade < 12
     SQL
     
-    
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end 
     
   end 
+  
   
   def save
     sql = <<-SQL
