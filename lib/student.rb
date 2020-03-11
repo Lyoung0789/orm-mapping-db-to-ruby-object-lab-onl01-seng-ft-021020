cd2f10b3eda360c2b6b5577f18cb39d76ac1c393
Lester Young <lyoung070789@gmail.com>
2020-03-11 16:17:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,12 +77,12 @@ class Student
      sql = <<-SQL
       SELECT * 
       FROM students 
-      WHERE grade = 10 LIMIT 1 
+      WHERE grade = 10 
     SQL
     # binding.pry 
     DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
-    end
+    end.first
   end   
   
   
